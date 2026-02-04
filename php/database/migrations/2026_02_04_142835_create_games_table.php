f58498e39fff8714c6001e86d39da72e92727570
Nyeko <kuro.kun972@gmail.com>
2026-02-04 16:39:16 +0100

    add login and register

--- php/database/migrations/2026_02_04_142835_create_games_table.php
+++ php/database/migrations/2026_02_04_142835_create_games_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
             $table->string('title');
             $table->text('description');
             $table->date('release_date');
+
+            
+            $table->foreignId('user_id')
+                  ->constrained()
+                  ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
